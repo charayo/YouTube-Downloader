166ebaf3e9d28cbed6529d0f0799a0b3d60f4100
charayo <69734231+charayo@users.noreply.github.com>
2021-11-25 14:42:56 -0500

    complete app

--- main.py
+++ main.py
@@ -3,5 +3,24 @@ from pytube import YouTube
 
 root = Tk()
 root.geometry('500x300')
-root.resizable(0,0)
-root.title("DataFlair-youtube video downloader")
+root.resizable(0, 0)
+root.title("Ayodeji-youtube video downloader")
+Label(root, text='Youtube Video Downloader', font='arial 20 bold').pack()
+Label(root, text='by Ayodeji', font='arial 15 bold').pack()
+link = StringVar()
+
+Label(root, text='Oya Paste Copied Youtube Link Here:', font='arial 15').pack()
+link_enter = Entry(root, width=70, textvariable=link).place(x=32, y=110)
+
+
+def downloader():
+    url = YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text='DOWNLOADED', font='arial 15').place(x=180, y=210)
+
+
+Button(root, text='DOWNLOAD', font='arial 15 bold', bg='pale violet red', padx=2, command=downloader).place(x=180,
+                                                                                                            y=150)
+
+root.mainloop()
